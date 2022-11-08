338b83a6b78e47690580b6a8c11718de0bf4bb01
jojolepro <jojolepromain@gmail.com>
2022-11-08 18:41:40 -0500

    set stage1 by default.

--- build.zig
+++ build.zig
@@ -7,21 +7,28 @@ pub fn build(b: *std.build.Builder) void {
 
     const lib = b.addStaticLibrary("archecs", "src/main.zig");
     lib.setBuildMode(mode);
+    lib.use_stage1 = true;
     lib.install();
 
     const main_tests = b.addTest("src/main.zig");
     main_tests.setBuildMode(mode);
+    main_tests.use_stage1 = true;
     const entity_tests = b.addTest("src/entity.zig");
     entity_tests.setBuildMode(mode);
+    entity_tests.use_stage1 = true;
     const archetype_tests = b.addTest("src/archetype.zig");
     archetype_tests.setBuildMode(mode);
+    archetype_tests.use_stage1 = true;
     const archetypes_tests = b.addTest("src/archetypes.zig");
     archetypes_tests.setBuildMode(mode);
+    archetypes_tests.use_stage1 = true;
     const dispatcher_tests = b.addTest("src/dispatcher.zig");
     dispatcher_tests.setBuildMode(mode);
     dispatcher_tests.test_evented_io = true;
+    dispatcher_tests.use_stage1 = true;
     const comptime_utils_tests = b.addTest("src/comptime_utils.zig");
     comptime_utils_tests.setBuildMode(mode);
+    comptime_utils_tests.use_stage1 = true;
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
